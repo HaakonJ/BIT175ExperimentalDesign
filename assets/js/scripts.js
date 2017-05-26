499b99bcb0865d682d904d8cf4b38ec784430003
HaakonJ <haakon@haakonjakobson.com>
2017-05-26 14:19:19 -0700

    navigation added layout changes to scss

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,23 @@
 // JavaScript Document
-var myvariable = 'Hello';
-alert(myvariable);
 
-var var2 = 'World!!';
+var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "100%"}});
 
-alert(var2);
+						// build scenes
+						new ScrollMagic.Scene({triggerElement: "#parallax1"})
+										.setTween("#parallax1 > div", {y: "50%", ease: Linear.easeNone})
+										
+										.addTo(controller);
 
-var nthing = 'Stuff';
+						new ScrollMagic.Scene({triggerElement: "#parallax2"})
+										.setTween("#parallax2 > div", {y: "50%", ease: Linear.easeNone})
+										
+										.addTo(controller);
 
-alert(nthing);
+						new ScrollMagic.Scene({triggerElement: "#parallax3"})
+										.setTween("#parallax3 > div", {y: "50%", ease: Linear.easeNone})
+										
+										.addTo(controller);
+						new ScrollMagic.Scene({triggerElement: "#parallax4"})
+										.setTween("#parallax4 > div", {y: "50%", ease: Linear.easeNone})
+										
+										.addTo(controller);
